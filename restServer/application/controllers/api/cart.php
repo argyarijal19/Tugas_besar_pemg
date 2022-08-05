412f2d1fa59ed76949f029fe3a61b660e78eb62c
argyarijal19 <bosargya@gmail.com>
2022-08-05 21:47:24 +0700

    add cart to checkout

--- restServer/application/controllers/api/cart.php
+++ restServer/application/controllers/api/cart.php
@@ -31,4 +31,26 @@ class Cart extends RESTController
             ], 404);
         }
     }
+
+    function index_post()
+    {
+        $insert = $this->Cart_Model->postcart();
+        if ($insert) {
+            // $this->response($data, 200);
+            $this->response(['status' => 'success', 'message' => 'Cart Berhasil Di Tambahkan !', 'data' => $insert], 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
+    function index_delete()
+    {
+        $id = $this->delete('id_produk');
+        $delete = $this->Cart_Model->deletecart($id);
+        if ($delete) {
+            $this->response(array('status' => true, 'message' => 'Cart has been DELETED !!!'), 202);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
 }
--- restServer/application/models/Cart_Model.php
+++ restServer/application/models/Cart_Model.php
@@ -10,10 +10,11 @@ class Cart_Model extends CI_Model
 	{
 		if ($id_pelanggan) {
             $this->db->where('c.id_pelanggan', $id_pelanggan);            
-            $this->db->select('c.id_pelanggan, c.id_cart, c.id_produk, c.quantity, l.nama_pelanggan, p.harga_produk, p.nama_produk, (c.quantity * p.harga_produk) as total_harga');
+            $this->db->select('c.id_pelanggan, c.id_cart, c.id_produk, SUM(c.quantity) as quantity,p.foto_produk, l.nama_pelanggan, p.harga_produk, p.nama_produk, (SUM(c.quantity) * p.harga_produk) as total_harga');
             $this->db->from('cart c');
             $this->db->join('pelanggan l', 'l.id_pelanggan=c.id_pelanggan');
             $this->db->join('produk p', 'p.id_produk=c.id_produk');
+            $this->db->group_by('p.id_produk');
             $cart = $this->db->get()->result();
             return $cart;
         } else {
@@ -25,4 +26,22 @@ class Cart_Model extends CI_Model
             return $cart;
         }
 	}
+
+    public function postcart()
+    {
+        $data = array(
+            'id_pelanggan' => $this->input->post('id_pelanggan'),
+            'id_produk' => $this->input->post('id_produk'),
+            'quantity' => $this->input->post('quantity')
+        );
+        $insert = $this->db->insert('cart', $data);
+        return $insert;
+    }
+
+    public function deletecart($id)
+    {
+        $this->db->where('id_produk', $id);
+        $delete = $this->db->delete('cart');
+        return $delete;
+    }
 }
