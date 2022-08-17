3438a61e1cc70b824a5e0f0ac14e9538929e684d
argyarijal19 <bosargya@gmail.com>
2022-08-17 21:45:08 +0700

    #update db

--- onlineshop.sql
+++ onlineshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2022 at 01:07 PM
+-- Generation Time: Aug 17, 2022 at 04:43 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -47,11 +47,11 @@ INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama_lengkap`, `foto_p
 -- --------------------------------------------------------
 
 --
--- Table structure for table `chart`
+-- Table structure for table `cart`
 --
 
-CREATE TABLE `chart` (
-  `id_chart` int(11) NOT NULL,
+CREATE TABLE `cart` (
+  `id_cart` int(11) NOT NULL,
   `id_pelanggan` int(11) NOT NULL,
   `id_produk` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
@@ -59,12 +59,26 @@ CREATE TABLE `chart` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `chart`
+-- Dumping data for table `cart`
 --
 
-INSERT INTO `chart` (`id_chart`, `id_pelanggan`, `id_produk`, `quantity`, `status`) VALUES
-(1, 29, 27, 1, ''),
-(2, 29, 29, 2, '');
+INSERT INTO `cart` (`id_cart`, `id_pelanggan`, `id_produk`, `quantity`, `status`) VALUES
+(23, 31, 36, 1, 'checkout'),
+(24, 31, 28, 1, 'checkout'),
+(25, 31, 27, 1, 'checkout'),
+(26, 31, 27, 1, 'checkout'),
+(27, 31, 28, 1, 'checkout'),
+(28, 31, 27, 1, 'checkout'),
+(29, 31, 28, 1, 'checkout'),
+(30, 31, 27, 1, 'checkout'),
+(31, 32, 27, 1, 'checkout'),
+(32, 32, 27, 1, 'checkout'),
+(33, 32, 35, 1, 'checkout'),
+(34, 32, 36, 1, 'checkout'),
+(35, 32, 36, 1, 'checkout'),
+(36, 32, 35, 1, 'checkout'),
+(37, 32, 36, 1, 'checkout'),
+(38, 32, 35, 1, 'checkout');
 
 -- --------------------------------------------------------
 
@@ -78,20 +92,26 @@ CREATE TABLE `checkout` (
   `tanggal_pembelian` date NOT NULL,
   `total_pembelian` int(11) NOT NULL,
   `id_ongkir` int(11) NOT NULL,
-  `alamat_rumah` varchar(100) NOT NULL
+  `alamat_rumah` varchar(100) NOT NULL,
+  `status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `checkout`
 --
 
-INSERT INTO `checkout` (`id_checkout`, `id_pelanggan`, `tanggal_pembelian`, `total_pembelian`, `id_ongkir`, `alamat_rumah`) VALUES
-(38, 29, '2022-07-30', 200000, 2, 'jl ciwaruga samping komplek mustika residence'),
-(39, 30, '2021-01-28', 60000, 1, 'kuningan jawbarat'),
-(40, 29, '2021-01-29', 100000, 1, 'desa sindangjaya kec mangunjaya kab pangandaran'),
-(41, 31, '2021-01-29', 200000, 2, 'sarijadi desa ciwaruga'),
-(42, 30, '2021-01-29', 80000, 1, 'desa sindangjaya'),
-(44, 30, '2022-08-04', 55000, 2, 'bandung');
+INSERT INTO `checkout` (`id_checkout`, `id_pelanggan`, `tanggal_pembelian`, `total_pembelian`, `id_ongkir`, `alamat_rumah`, `status`) VALUES
+(58, 31, '2022-08-06', 125000, 2, '', 'proses'),
+(59, 31, '2022-08-06', 55000, 2, 'jl. ciwaruga bandung', 'proses'),
+(60, 31, '2022-08-06', 70000, 2, '', 'proses'),
+(61, 31, '2022-08-06', 55000, 2, '', 'proses'),
+(62, 31, '2022-08-06', 70000, 2, '', 'proses'),
+(63, 31, '2022-08-06', 55000, 2, 'jl. ciwaruga bandung', 'proses'),
+(64, 32, '2022-08-06', 55000, 2, 'halo semuanya ', 'proses'),
+(65, 32, '2022-08-11', 100000, 2, '', 'proses'),
+(66, 32, '2022-08-11', 100000, 2, '', 'proses'),
+(67, 32, '2022-08-11', 560000, 2, '', 'proses'),
+(68, 32, '2022-08-11', 330000, 2, '', 'proses');
 
 -- --------------------------------------------------------
 
@@ -109,7 +129,6 @@ CREATE TABLE `kategori` (
 --
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
-(1, 'Cardigan'),
 (2, 'baju'),
 (5, 'gamis update'),
 (6, 'kemeja'),
@@ -138,7 +157,8 @@ CREATE TABLE `keys` (
 
 INSERT INTO `keys` (`id`, `user_id`, `key`, `level`, `ignore_limits`, `is_private_key`, `ip_addresses`, `date_created`) VALUES
 (1, 0, 'MyKey', 0, 0, 0, NULL, 0),
-(2, 1, 'zJixXtP64Y', 1, 0, 0, NULL, 0);
+(2, 1, 'zJixXtP64Y', 1, 0, 0, NULL, 0),
+(3, 4, 'DXZHuIYOgR', 1, 0, 0, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -181,8 +201,8 @@ CREATE TABLE `pelanggan` (
 INSERT INTO `pelanggan` (`id_pelanggan`, `email_pelanggan`, `password_pelanggan`, `nama_pelanggan`, `telepon_pelanggan`) VALUES
 (29, 'emans6050@gmail.com', '$2y$10$Le0WN.SWqd3pqYTbni.PHe.rLOaJHxSwia42qmNFRwZHpHjmNLo2.', 'bimo arga ', '0814132112'),
 (30, 'abah@gmail.com', '$2y$10$nKp2K/pv0Z5GfOoZgOsKY.CGDH5RTnaA/mZcoIFhv1FWJ8xO64iNu', 'abah', '0813136717'),
-(31, 'dani@gmail.com', '$2y$10$5bsfWxtQE.2fRTEXc5oaCubx0aRHLNdkR0HddXt8XQp3pNtTbA85i', 'dani maripola', '081545635'),
-(32, 'emans6050@gmail.com', '$2y$10$UoXEDNflmU3UVBAEqXpSTeJtfemDLAJiiGGhteOqJE5R4JjTukdQu', 'argya rjal', '081394728945');
+(31, 'dani@gmail.com', '123', 'dani maripola', '081545635'),
+(32, 'bosargya@gmail.com', '123', 'argya rjal', '081394728945');
 
 -- --------------------------------------------------------
 
@@ -241,14 +261,13 @@ CREATE TABLE `produk` (
 --
 
 INSERT INTO `produk` (`id_produk`, `id_kategori`, `nama_produk`, `harga_produk`, `berat_produk`, `foto_produk`, `deskripsi_produk`, `stok_produk`) VALUES
-(27, 2, 'cardigan pria BDAH AH', 55000, 300, 'https://hm-media-prod.s3.amazonaws.com/pub/media/catalog/product/medium/edd641bdc987c5a7627fdbd9c1abdfe2037c9911_xxl-1.jpg', 'cardigan pria yang elegant dan membuat kita terliat fashionable dengan bahan sutra sehingga lembut saat di gunakan ', 21),
 (28, 1, 'cardigan all gender', 70000, 350, 'https://image.made-in-china.com/202f0j00hCURWvYMgboP/European-and-American-Autumn-and-Winter-New-V-Neck-Button-Striped-Sweater-Cardigan.jpg', 'cardigan ini cocok untuk pria maupun wanita dengan bahan kain rajutan tangan sehingga terlihat lebih elegant dan nyaman di gunakan dalam cuaca panas atau pun dingin                      ', 0),
 (29, 1, 'cardigan merah jambufsdsf', 50000, 150, 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//101/MTA-19182818/no_brand_cardigan-atasan_importlengan_panjang_sweater-fashion_wanita-sweater_wanita_full13_gii1v3x0.jpg', 'cardigan wanita yang lucu dan imut dengan bahan yang lembut mampu membuat anda terlihat lebih feminim       ', 3),
 (30, 1, 'cardigan musim dingin UPDATE', 100000, 200, 'https://s4.bukalapak.com/img/9252523702/large/baju_musim_dingin_sale_JAKET_OUTER_CARDIGAN_FASHION_WANITA_K.png', 'cardigan ini cocok untuk musim pancaroba karena memiliki bahan dari kain wol sehingga bisa membuat anda hangat sehangat anda dalam pelukan nya               ', 4),
 (31, 1, 'cardigan vanila', 65000, 400, 'https://www.ilovetall.com/media/image/27/ae/f8/ILOVETALL-Damen-Cardigan-offen-extralang-Langgr%C3%B6sse-vanilla-gelb-2621-2.jpg', 'cardigan nyaman dengan warna yang relatif lebih sejuk dan hangat saaat di gunakan         ', 5),
 (33, 1, 'cardigan kekinian', 70000, 400, 'https://s1.thcdn.com/productimg/1600/1600/13331780-1024898225049843.jpg', 'cardigan yang sangat cocok untuk wanita yang luar biasa               ', 4),
 (34, 0, 'kerudung muslimah update', 50000, 150, 'https://cf.shopee.co.id/file/a327492da9199d66a25c6dff3ccb47e8', 'kerudung segi empat untuk muslimah yang ingin berhijrah dengan corak bunga yang indah                                                                ', 4),
-(35, 2, 'kerudung muslimah edan parah', 100000, 119, 'https://s0.bukalapak.com/img/06083920241/large/KHIMAR_TERBARU_JILBAB_MUSLIMAH_KERUDUNG_TRENDY_KHIMAR_ANNISA.jpg', 'kerudung dengan bahan sutra yang lembut dan tidak gatal saat di kenakan                      ', 10),
+(35, 0, 'kerudung muslimah edan parah', 200000, 119, 'https://s0.bukalapak.com/img/06083920241/large/KHIMAR_TERBARU_JILBAB_MUSLIMAH_KERUDUNG_TRENDY_KHIMAR_ANNISA.jpg', 'kerudung dengan bahan sutra yang lembut dan tidak gatal saat di kenakan                      ', 10),
 (36, 2, 'cardiBy.me segi4 for muslimah', 230000, 500, 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//97/MTA-7370196/mystory_mystory_larissa_hijab_muslimah_full04_tvy79ci5.jpg', 'couple muslimah style untuk sahabat atau hadiah kepada adik sangat cocok sekali dengan design yang elegant dan mewah dan bahan yang lembut        ', 9),
 (37, 2, 'kerudung muslimah segi empat', 100000, 150, 'https://a.ipricegroup.com/media/Maria/koleksi_hijab_untuk_wanita_muslim.png', 'bahan ringan dan sejuk saat di kenakan dan memiliki tingkan kelembutan yang super sotf sehingga bisa membuat anda betah memakai kerudung ini meskipun di kenakan seharian       ', 11),
 (38, 2, 'Kerudung cardiBy.me muslimah Update', 120000, 140, 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//94/MTA-6781261/mydailyhijab_mydailyhijab_segi_4_voal_aisyah_lasercut_hijab_muslimah_full06_ozwtiit7.jpg', 'bahan karet gelang yang istimewa sehingga bisa membuat para pengguna nya dapat ketenangan dan kedaimaian        ', 5);
@@ -270,7 +289,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`) VALUES
-(1, 'argya', '123');
+(1, 'argya', '123'),
+(3, 'argya', '123'),
+(4, 'argyarijalrafi', '123');
 
 --
 -- Indexes for dumped tables
@@ -283,10 +304,10 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
--- Indexes for table `chart`
+-- Indexes for table `cart`
 --
-ALTER TABLE `chart`
-  ADD PRIMARY KEY (`id_chart`);
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id_cart`);
 
 --
 -- Indexes for table `checkout`
@@ -347,16 +368,16 @@ ALTER TABLE `admin`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
--- AUTO_INCREMENT for table `chart`
+-- AUTO_INCREMENT for table `cart`
 --
-ALTER TABLE `chart`
-  MODIFY `id_chart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `cart`
+  MODIFY `id_cart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `checkout`
 --
 ALTER TABLE `checkout`
-  MODIFY `id_checkout` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id_checkout` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- AUTO_INCREMENT for table `kategori`
@@ -368,7 +389,7 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `keys`
 --
 ALTER TABLE `keys`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `ongkir`
@@ -398,7 +419,7 @@ ALTER TABLE `produk`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
